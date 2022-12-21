2f114f782d4dad198ebb6f8c67b6e53a87269f11
Ke Duong <kelyduong@gmail.com>
2022-12-21 12:45:34 +1100

    update calendar stuff

--- src/components/CalendarForm.jsx
+++ src/components/CalendarForm.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
 import Datepicker from "react-tailwindcss-datepicker";
 import "../css_files/calendar.css";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import {
+  CalendarPicker,
+  DesktopDatePicker,
+  LocalizationProvider,
+} from "@mui/x-date-pickers";
+import { TextField } from "@mui/material";
+import dayjs from "dayjs";
 
 function CalendarForm() {
   const [date, setDate] = useState(new Date());
   const [value, setValue] = useState(new Date());
+  const [valueMUI, setValueMUI] = useState(new Date());
+  const [valueMUI2, setValueMUI2] = useState(dayjs());
   return (
     <div>
       {/* Reference: https://blog.logrocket.com/react-calendar-tutorial-build-customize-calendar/ */}
+      {/* A simple React library */}
       <div name="calendarType1">
         <h1 className="text-center text-4xl">Calendar type 1</h1>
         <div className="calendar-container flex justify-center">
-          <Calendar onChange={setDate} value={date} minDetail="year" />
+          <Calendar
+            onChange={setDate}
+            value={date}
+            minDetail="year"
+            next2Label={null}
+            prev2Label={null}
+            showNeighboringMonth={false}
+            formatShortWeekday={(locale, date) =>
+              date.toString().substring(0, 1)
+            }
+          />
         </div>
         <div>
           <p className="text-center">
@@ -28,16 +48,8 @@ function CalendarForm() {
           </button>
         </div> */}
       </div>
-
-      {/* ---------------------------------------- */}
-      {/* Reference: https://reactjsexample.com/a-modern-date-range-picker-component-for-react-using-tailwind-3-and-dayjs/ 
-      Apparently this one uses Tailwind 3 and dayjs in it --> Support Typescript*/}
-      <div>
-        <h1 className="text-center text-4xl">Calendar type 2</h1>
-        <div className="flex justify-center">
-          <Datepicker value={value} onChange={setValue} showShortcuts={true} />
-        </div>
-      </div>
+      {/* ------------------------------------------- */}
+      {/* Reference for Google API: https://fusebit.io/blog/google-calendar-react/?utm_source=www.google.com&utm_medium=referral&utm_campaign=none */}
     </div>
   );
 }
